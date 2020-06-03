bfcfb089a2ab6deae803e04b709ed67bd2d8a35b
dhc-leizhong <lei1.zhong@dhc.com.cn>
2020-06-03 21:13:48 +0800

    修改storebook

--- src/components/Menu/menu.stories.tsx
+++ src/components/Menu/menu.stories.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import Menu from './menu'
-import MenuItem from './menuItem'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Menu from "./index";
+import MenuItem from "./menuItem";
 
 export const defaultMenu = () => (
-  <Menu defaultIndex='0' onSelect={(index) => {action(`clicked ${index} item`)}} >
-    <MenuItem>
-      cool link
-    </MenuItem>
-    <MenuItem disabled>
-      disabled
-    </MenuItem> 
-    <MenuItem>
-      cool link 2
-    </MenuItem> 
+  <Menu
+    defaultIndex="0"
+    onSelect={(index) => {
+      action(`clicked ${index} item`);
+    }}
+  >
+    <Menu.Item>cool link</Menu.Item>
+    <MenuItem disabled>disabled</MenuItem>
+    <Menu.SubMenu title="dropDown">
+      <MenuItem>cool link 21</MenuItem>
+      <MenuItem>cool link 21</MenuItem>
+    </Menu.SubMenu>
+    <MenuItem>cool link 2</MenuItem>
   </Menu>
-)
+);
 
-storiesOf('Menu Component', module)
-.add('Menu', defaultMenu )
+storiesOf("Menu Component", module).add("Menu", defaultMenu);
